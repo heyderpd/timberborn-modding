746c69ab5e99ef2db8f203fa1f01d69fa9077bc4
Heyder <heyderpd@gmail.com>
2024-11-03 19:58:56 +0000

    create first model and basic script

---
+++ timberborn-modding/Assets/Mods/Pipe/Scripts/PipeWaterInput.cs
@@ -0,0 +1,94 @@
+﻿using Bindito.Core;
+using Timberborn.BaseComponentSystem;
+using Timberborn.WaterSystem;
+using Timberborn.BlockSystem;
+using UnityEngine;
+
+namespace Mods.Pipe.Scripts
+{
+  internal class PipeWaterInput : BaseComponent
+  {
+
+    [SerializeField]
+    private string _inputCoordinates; // Front Back , Left Right , Top Botton
+
+    [SerializeField]
+    private Vector3Int _waterCoordinates;
+
+    private BlockObject _blockObject;
+
+    private Vector3Int _waterCoordinatesTransformed;
+
+    private IWaterService _waterService;
+
+    private IThreadSafeWaterMap _threadSafeWaterMap;
+
+    public bool IsUnderwater
+    {
+      get
+      {
+        return _threadSafeWaterMap.CellIsUnderwater(_waterCoordinatesTransformed);
+      }
+    }
+
+    public float AvailableWater
+    {
+      get
+      {
+        float totalWaterAmount = _threadSafeWaterMap.WaterHeightOrFloor(_waterCoordinatesTransformed);
+        return totalWaterAmount - (float)(_waterCoordinatesTransformed.z);
+      }
+    }
+
+    public float AvailableContaminatedWater
+    {
+      get
+      {
+        float availableWater = this.AvailableWater;
+        float contaminationPercentage = _threadSafeWaterMap.ColumnContamination(_waterCoordinatesTransformed);
+        return availableWater * contaminationPercentage;
+      }
+    }
+
+    [Inject]
+    public void InjectDependencies(IWaterService waterService, IThreadSafeWaterMap threadSafeWaterMap)
+    {
+      _waterService = waterService;
+      _threadSafeWaterMap = threadSafeWaterMap;
+    }
+
+    public void Awake()
+    {
+      _blockObject = GetComponentFast<BlockObject>();
+    }
+
+    public void Start()
+    {
+      _waterCoordinatesTransformed = _blockObject.Transform(_waterCoordinates);
+    }
+
+    public void RemoveWater(float cleanWater, float contaminatedWater)
+    {
+      if (cleanWater > 0f)
+      {
+        _waterService.RemoveCleanWater(_waterCoordinatesTransformed, cleanWater);
+      }
+      if (contaminatedWater > 0f)
+      {
+        _waterService.RemoveContaminatedWater(_waterCoordinatesTransformed, contaminatedWater);
+      }
+    }
+
+    public void AddWater(float cleanWater, float contaminatedWater)
+    {
+      if (cleanWater > 0f)
+      {
+        _waterService.AddCleanWater(_waterCoordinatesTransformed, cleanWater);
+      }
+      if (contaminatedWater > 0f)
+      {
+        _waterService.AddContaminatedWater(_waterCoordinatesTransformed, contaminatedWater);
+      }
+    }
+  }
+}
---
+++ timberborn-modding/Assets/Mods/Pipe/Scripts/PipeWaterMove.cs
@@ -0,0 +1,97 @@
+﻿using Timberborn.BlockSystem;
+using Timberborn.EntitySystem;
+using Timberborn.Persistence;
+using Timberborn.TickSystem;
+using UnityEngine;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Mods.Pipe.Scripts
+{
+
+  internal class PipeBuilding : TickableComponent,
+                                IInitializableEntity,
+                                IFinishedStateListener,
+                                IPersistentEntity {
+
+    private bool ready = false;
+
+    private float _waterDiffLimit = 0.02f;
+
+    private float _waterCapacity = 0.25f;
+
+    private List<PipeWaterInput> _waterInputs;
+
+    private PipeWaterInput _waterInputA;
+
+    private PipeWaterInput _waterInputB;
+
+    public void Awake()
+    {
+      List<PipeWaterInput> _waterInputs = new List<PipeWaterInput>();
+      GetComponentsFast<PipeWaterInput>(_waterInputs);
+      _waterInputA = _waterInputs.First();
+      _waterInputB = _waterInputs.Last();
+      ((Behaviour)this).enabled = false;
+    }
+
+    public void InitializeEntity()
+    {
+      ready = true;
+    }
+
+    public void DeleteEntity()
+    {
+      ready = false;
+    }
+
+    public override void Tick()
+    {
+      if (!ready)
+      {
+        return;
+      }
+      float waterA = _waterInputA.AvailableWater;
+      float waterB = _waterInputB.AvailableWater;
+      if (waterA <= 0 && waterB <= 0)
+      {
+        return;
+      }
+      float waterDiff = Mathf.Abs(waterA - waterB) / 2;
+      if (waterDiff < _waterDiffLimit)
+      {
+        return;
+      }
+      waterDiff = Mathf.Min(waterDiff, _waterCapacity);
+      if (waterA > waterB)
+      {
+        MoveWater(_waterInputA, _waterInputB, waterDiff);
+      } else
+      {
+        MoveWater(_waterInputB, _waterInputA, waterDiff);
+      }
+    }
+
+    public void Save(IEntitySaver entitySaver) { }
+
+    public void Load(IEntityLoader entityLoader) { }
+
+    public void OnEnterFinishedState()
+    {
+      ((Behaviour)this).enabled = true;
+    }
+
+    public void OnExitFinishedState()
+    {
+      ((Behaviour)this).enabled = false;
+    }
+
+    public void MoveWater(PipeWaterInput waterHigh, PipeWaterInput waterLower, float availableWater)
+    {
+      float contaminatedWater = waterHigh.AvailableContaminatedWater;
+      float cleanWater = availableWater - contaminatedWater;
+      waterHigh.RemoveWater(cleanWater, contaminatedWater);
+      waterLower.AddWater(cleanWater, contaminatedWater);
+    }
+  }
+}
