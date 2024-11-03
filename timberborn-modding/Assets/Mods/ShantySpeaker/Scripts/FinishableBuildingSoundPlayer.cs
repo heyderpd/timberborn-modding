dd06c934543dcbeb7f4343f635760380be8f0aee
Heyder <heyderpd@gmail.com>
2024-11-03 19:57:10 +0000

    make first test using shanty

---
+++ timberborn-modding/Assets/Mods/ShantySpeaker/Scripts/FinishableBuildingSoundPlayer.cs
@@ -0,0 +1,26 @@
+﻿using Bindito.Core;
+using Timberborn.BaseComponentSystem;
+using Timberborn.BlockSystem;
+using Timberborn.CoreSound;
+using Timberborn.SoundSystem;
+using UnityEngine;
+
+namespace Mods.ShantySpeaker.Scripts {
+  internal class FinishableBuildingSoundPlayer : BaseComponent,
+                                                 IFinishedStateListener {
+
+    private ISoundSystem _soundSystem;
+
+    [Inject]
+    public void InjectDependencies(ISoundSystem soundSystem) {
+      _soundSystem = soundSystem;
+    }
+
+    public void OnEnterFinishedState() {
+    }
+
+    public void OnExitFinishedState() {
+    }
+
+  }
+}
