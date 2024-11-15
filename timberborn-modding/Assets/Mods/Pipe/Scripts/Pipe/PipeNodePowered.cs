b6f9308e9b6e3ff26899ed47c6aaba6ad0efe28c
Heyder <heyderpd@gmail.com>
2024-11-15 14:59:19 +0000

    add power togler

--- timberborn-modding/Assets/Mods/Pipe/Scripts/Pipe/PipeNodePowered.cs
+++ timberborn-modding/Assets/Mods/Pipe/Scripts/Pipe/PipeNodePowered.cs
@@ -7,6 +7,8 @@ namespace Mods.OldGopher.Pipe.Scripts
   {
     private MechanicalNode mechanicalNode;
 
+    public bool Active = true;
+
     private bool GraphValid => mechanicalNode?.Graph?.Valid ?? false;
 
     private bool NodeInvalid => mechanicalNode == null;
@@ -25,34 +27,28 @@ namespace Mods.OldGopher.Pipe.Scripts
 
     public void DisablePowerConsumption()
     {
-      ModUtils.Log($"[PipeNodePowered] 01 DisablePowerConsumption will Disable : checkA={NodeInvalid} checkB={GraphValid}");
       if (NodeInvalid)
         return;
       mechanicalNode.Active = false;
       if (GraphValid)
         mechanicalNode.UpdateInput(0);
-      ModUtils.Log($"[PipeNodePowered] 02 DisablePowerConsumption is Disabled");
     }
 
     public void EnablePowerConsumption()
     {
-      ModUtils.Log($"[PipeNodePowered] 01 EnablePowerConsumption will Enable : checkA={NodeInvalid} checkB={GraphValid}");
       if (NodeInvalid)
         return;
       mechanicalNode.Active = true;
       if (GraphValid)
         mechanicalNode.UpdateInput(1);
-      ModUtils.Log($"[PipeNodePowered] 02 EnablePowerConsumption is Enabled");
     }
 
     public float PowerEfficiency
     {
       get
       {
-        ModUtils.Log($"[PipeNodePowered] 01 PowerEfficiency will get value : checkA={NodeInvalid} checkB={NodeActiveAndPowered}");
-        if (NodeInvalid || !NodeActiveAndPowered)
+        if (!Active || NodeInvalid || !NodeActiveAndPowered)
           return 0f;
-        ModUtils.Log($"[PipeNodePowered] 02 PowerEfficiency={mechanicalNode.PowerEfficiency}");
         return mechanicalNode.PowerEfficiency;
       }
     }
