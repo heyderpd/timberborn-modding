3ae603180d8142c4b8b982c1cc6110d3f5a96777
Heyder <heyderpd@gmail.com>
2024-11-29 00:13:34 +0000

    fix nativeObstacle usage

--- timberborn-modding/Assets/Mods/Pipe/Scripts/Radar/WaterObstacleMap.cs
+++ timberborn-modding/Assets/Mods/Pipe/Scripts/Radar/WaterObstacleMap.cs
@@ -6,9 +6,9 @@ namespace Mods.OldGopher.Pipe
 {
   internal class WaterObstacleMap
   {
-    private readonly HashSet<Vector3Int> virtualObstacle;
+    private readonly HashSet<Vector3Int> virtualObstacle = new HashSet<Vector3Int>();
 
-    private readonly HashSet<Vector3Int> nativeObstacle;
+    private readonly BlockableCount<Vector3Int> nativeObstacle = new BlockableCount<Vector3Int>();
 
     private bool IsInvalid(Vector3Int coordinate)
     {
@@ -30,22 +30,21 @@ namespace Mods.OldGopher.Pipe
 
     public void SetNative(Vector3Int coordinate)
     {
-      if (IsInvalid(coordinate) || nativeObstacle.Contains(coordinate))
+      if (IsInvalid(coordinate))
         return;
-      nativeObstacle.Add(coordinate);
+      nativeObstacle.Block(coordinate);
     }
 
     public void UnsetNative(Vector3Int coordinate)
     {
-      if (nativeObstacle.Contains(coordinate))
-        nativeObstacle.Remove(coordinate);
+      nativeObstacle.Unblock(coordinate);
     }
 
     public bool Exist(Vector3Int coordinate)
     {
       if (IsInvalid(coordinate))
         return false;
-      return virtualObstacle.Contains(coordinate) || nativeObstacle.Contains(coordinate);
+      return nativeObstacle.Contains(coordinate);
     }
 
     public bool NotExist(Vector3Int coordinate)
