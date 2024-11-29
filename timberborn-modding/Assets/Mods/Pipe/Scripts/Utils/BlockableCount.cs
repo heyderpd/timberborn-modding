014e349c61f4727f2c2d8dbaa4e621c3d018af00
Heyder <heyderpd@gmail.com>
2024-11-29 00:13:10 +0000

    simplify class

--- timberborn-modding/Assets/Mods/Pipe/Scripts/Utils/BlockableCount.cs
+++ timberborn-modding/Assets/Mods/Pipe/Scripts/Utils/BlockableCount.cs
@@ -1,39 +1,20 @@
 using System.Collections.Generic;
-using System;
-using Timberborn.BaseComponentSystem;
-using Timberborn.Navigation;
-using UnityEngine;
-using Castle.DynamicProxy.Generators.Emitters.SimpleAST;
-using Timberborn.BlockSystem;
 
 namespace Mods.OldGopher.Pipe
 {
-  public class BlockableCount<T>
+  internal class BlockableCount<T>
   {
     private readonly Dictionary<T, int> Blockers = new Dictionary<T, int>();
 
-    public event EventHandler<T> Blocked;
-
-    public event EventHandler<T> Unblocked;
-
-    public event EventHandler BuildingBlocked;
-
-    public event EventHandler BuildingUnblocked;
-
-    private void BlockEvent(object sender, T reference)
+    public void Block(T reference)
     {
       if (Blockers.ContainsKey(reference))
         Blockers[reference] += 1;
       else
         Blockers.Add(reference, 1);
-      if (Blockers.TryGetValue(reference, out var value))
-      {
-        if (value == 1)
-          BuildingBlocked?.Invoke(this, EventArgs.Empty);
-      }
     }
 
-    private void UnblockEvent(object sender, T reference)
+    public void Unblock(T reference)
     {
       if (Blockers.TryGetValue(reference, out var value))
       {
@@ -41,21 +22,8 @@ namespace Mods.OldGopher.Pipe
         if (value > 0)
           Blockers[reference] = value;
         else
-        {
           Blockers.Remove(reference);
-          BuildingUnblocked?.Invoke(this, EventArgs.Empty);
-        }
       }
-    }
-
-    public void Block(T reference)
-    {
-      this.Blocked?.Invoke(this, reference);
-    }
-
-    public void Unblock(T reference)
-    {
-      this.Unblocked?.Invoke(this, reference);
     }
 
     public bool Contains(T reference)
