a5030baa39e7f77b541371d4795b017a1e6cae27
Heyder <heyderpd@gmail.com>
2024-11-30 00:14:33 +0000

    revert BlockableCount class

--- timberborn-modding/Assets/Mods/Pipe/Scripts/Utils/BlockableCount.cs
+++ timberborn-modding/Assets/Mods/Pipe/Scripts/Utils/BlockableCount.cs
@@ -1,87 +1,42 @@
-using Amazon.Runtime.Internal.Transform;
-using Castle.DynamicProxy.Generators.Emitters.SimpleAST;
 using System.Collections.Generic;
-using UnityEngine;
 
 namespace Mods.OldGopher.Pipe
 {
-  internal class BlockableItem
+  internal class BlockableCount<T>
   {
-    public WaterObstacle obstacle { get; private set; }
+    private readonly Dictionary<T, int> Blockers = new Dictionary<T, int>();
 
-    public int count;
-
-    public BlockableItem(WaterObstacle _obstacle)
+    public bool Block(T reference)
     {
-      obstacle = _obstacle;
-      count = 1;
-    }
-  }
-
-  internal class BlockableCount
-  {
-    private readonly Dictionary<Vector3Int, BlockableItem> Blockers = new Dictionary<Vector3Int, BlockableItem>();
-
-    public WaterObstacleState obstacleType;
-
-    public BlockableCount(bool fullObstacle)
-    {
-      obstacleType = fullObstacle
-        ? WaterObstacleState.FULL_OBSTACLE
-        : WaterObstacleState.PARTIAL_OBSTACLE;
-    }
-
-    private bool IsInvalid(WaterObstacle obstacle)
-    {
-      if (obstacle == null)
-        return false;
-      return obstacleType == obstacle.getType();
-    }
-
-    public bool Block(WaterObstacle reference)
-    {
-      if (IsInvalid(reference))
-        return false;
-      if (Blockers.TryGetValue(reference.Key, out var item))
+      if (Blockers.ContainsKey(reference))
       {
-        item.count += 1;
+        Blockers[reference] += 1;
         return false;
       }
-      Blockers.Add(reference.Key, new BlockableItem(reference));
+      Blockers.Add(reference, 1);
       return true;
     }
 
-    public bool Unblock(Vector3Int referenceKey)
+    public bool Unblock(T reference)
     {
-      if (referenceKey == null)
-        return false;
-      if (Blockers.TryGetValue(referenceKey, out var item))
+      if (Blockers.ContainsKey(reference))
       {
-        item.count -= 1;
-        if (item.count <= 0)
+        Blockers[reference] -= 1;
+        if (Blockers[reference] <= 0)
         {
-          Blockers.Remove(referenceKey);
+          Blockers.Remove(reference);
           return true;
         }
       }
       return false;
     }
 
-    public WaterObstacle Get(Vector3Int key)
+    public bool Contains(T reference)
     {
-      if (key == null)
-        return null;
-      if (Blockers.TryGetValue(key, out var item))
-        return item.obstacle;
-      return null;
-    }
-
-    public bool Contains(Vector3Int key)
-    {
-      if (key == null)
-        return false;
-      if (Blockers.TryGetValue(key, out var item))
-        return item.count > 0;
+      if (Blockers.TryGetValue(reference, out var value))
+      {
+        return value > 0;
+      }
       return false;
     }
   }
